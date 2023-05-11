d3e467ed296420e75e1bd45f05e67418c055c9b2
Piero899 <pieros.rossi@gmail.com>
2023-05-11 15:48:58 -0300

    Hice todo

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
 function establecerDireccion(keyCode) {
-{
-    if (keyCode == 65 || keyCode == 37)
-        return "izquierda"
-    else if (keyCode == 68 || keyCode == 39)
-        return "derecha"
-    else if (keyCode == 40 || keyCode == 83)
-        return "abajo"
-    else if (keyCode == 87 || keyCode == 38)
-        return "arriba"
+    if (keyCode == 65 || keyCode == 37) {
+        if (direccionViborita() !== "derecha")
+            return "izquierda";
+    } else if (keyCode == 68 || keyCode == 39) {
+        if (direccionViborita() !== "izquierda")
+            return "derecha";
+    } else if (keyCode == 40 || keyCode == 83) {
+        if (direccionViborita() !== "arriba")
+            return "abajo";
+    } else if (keyCode == 87 || keyCode == 38) {
+        if (direccionViborita() !== "abajo")
+            return "arriba";
+    }
+    return direccionViborita();
+}
+
+function viboritaPuedeComerManzana() {
+    if (posicionEnXDeLaViborita() === posicionEnXDeLaManzana() && posicionEnYDeLaViborita() === posicionEnYDeLaManzana()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function aumentarPuntaje() {
+    if (viboritaPuedeComerManzana()) {
+        puntaje++;
+        if (puntaje % 2 === 0) {
+            nivel++;
+            fps += 2;
+        }
+    }
+}
+
+function perdio(viborita) {
+    if (seEnredo(viborita) || posicionEnXDeLaViborita() >= anchoDeLaVentana() || posicionEnYDeLaViborita() >= altoDeLaVentana() || posicionEnXDeLaViborita()==0 || posicionEnYDeLaViborita()==0) {
+        return true;
+    } else {
+        return false;
     }
 }
--- respuestas.md
+++ respuestas.md
@@ -1,1 +1,3 @@
-## Respuesta punto 6
+## Respuesta punto 6 
+## || es como un O en la vida real, si el codigo hace esto O hace esto entonces se cumple lo que le pide
+## || && es como si fuera un Y de la vida real
